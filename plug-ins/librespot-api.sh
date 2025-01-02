9e7ffb556743d4e9411eb46816d0541fa494182c
Hai Ngo <111883679+hai-nd@users.noreply.github.com>
2025-01-02 11:48:10 +0700

    Delete plug-ins/librespot-api.sh

--- plug-ins/librespot-api.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-java -jar /usr/local/bin/librespot-api-1.6.3.jar --player.output="MIXER" --player.enableNormalisation=false --player.preferredAudioQuality="VERY_HIGH"
